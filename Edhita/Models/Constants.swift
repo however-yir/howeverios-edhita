ffd18c63272318eec4e10143c7ce11a6dac77e4a
however <however-yir@users.noreply.github.com>
2026-04-08 10:13:22 +0800

    fix: add debugGDPR constant for debug build

--- Edhita/Models/Constants.swift
+++ Edhita/Models/Constants.swift
@@ -4,5 +4,6 @@ struct Constants {
     static let adUnitID = "ca-app-pub-xxxxxxxxxxxxxxxx/nnnnnnnnnn"
     #if DEBUG
         static let enableAd = false
+        static let debugGDPR = false
     #endif
 }
